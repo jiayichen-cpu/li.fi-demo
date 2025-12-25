8e450afc609eebb48015354eade44a7c971f1273
jiayichen <jiayi.chen@intern.onekey.so>
2025-12-25 14:50:33 +0800

    fix: 更新 layout title 为 Year End Trading Carnival

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const notoSansSC = Noto_Sans_SC({
 });
 
 export const metadata: Metadata = {
-  title: "OneKey × LI.FI Year-End Trading Week",
+  title: "OneKey × LI.FI Year End Trading Carnival",
   description: "Complete cross-chain or Swap on OneKey App using LI.FI channel to participate in daily draws. Teams of 2-5 members can participate in the trading event, ranked by combined trading volume to win more rewards.",
   icons: {
     icon: '/onekey_icon_default_solid_green_black.svg',
